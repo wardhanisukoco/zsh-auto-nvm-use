1f149bf411b2b92aa5bb078493554b9475121d4e
wardhanisukoco <86147310+wardhanisukoco@users.noreply.github.com>
2024-05-31 04:19:51 +0700

    fix: update script use @rafaellucio

--- zsh-auto-nvm-use.plugin.zsh
+++ zsh-auto-nvm-use.plugin.zsh
@@ -1,11 +1,34 @@
-autoload -U add-zsh-hook
+
+# Auto load version node if exists .nvmrc
+[[ -n "$ZSH_NVM_AUTOLOAD" ]] || ZSH_NVM_AUTOLOAD=true
+
 load-nvmrc() {
-  if [[ -f .nvmrc && -r .nvmrc ]]; then
-    nvm use
-  elif [[ $(nvm version) != $(nvm version default)  ]]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
+	local node_version="$(nvm version)"
+	local nvmrc_path="$(nvm_find_nvmrc)"
+
+	if [ -n "$nvmrc_path" ]
+	then
+		local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+		if [ "$nvmrc_node_version" = "N/A" ]
+		then
+			nvm install
+		fi
+
+		if [ "$nvmrc_node_version" != "$node_version" ]
+		then
+			nvm use
+		fi
+	elif [ "$node_version" != "$(nvm version default)" ]  
+	then
+		echo "Reverting to nvm default version"
+		nvm use default
+	fi
 }
-add-zsh-hook chpwd load-nvmrc
-load-nvmrc
+
+if [[ "$ZSH_NVM_AUTOLOAD" == "true" ]]
+then
+	autoload -U add-zsh-hook
+	add-zsh-hook chpwd load-nvmrc
+	load-nvmrc
+fi
