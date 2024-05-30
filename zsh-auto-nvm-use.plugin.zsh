71fd7fce2086f2be42bb68b2037d9b38d7fa7203
wardhanisukoco <86147310+wardhanisukoco@users.noreply.github.com>
2024-05-31 04:57:19 +0700

    fix: update zsh-auto-nvm-use.plugin.zsh

--- zsh-auto-nvm-use.plugin.zsh
+++ zsh-auto-nvm-use.plugin.zsh
@@ -1,34 +1,24 @@
-
-# Auto load version node if exists .nvmrc
-[[ -n "$ZSH_NVM_AUTOLOAD" ]] || ZSH_NVM_AUTOLOAD=true
+# place this after nvm initialization!
+autoload -U add-zsh-hook
 
 load-nvmrc() {
-	local node_version="$(nvm version)"
-	local nvmrc_path="$(nvm_find_nvmrc)"
+  local nvmrc_path
+  nvmrc_path="$(nvm_find_nvmrc)"
 
-	if [ -n "$nvmrc_path" ]
-	then
-		local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version
+    nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
 
-		if [ "$nvmrc_node_version" = "N/A" ]
-		then
-			nvm install
-		fi
-
-		if [ "$nvmrc_node_version" != "$node_version" ]
-		then
-			nvm use
-		fi
-	elif [ "$node_version" != "$(nvm version default)" ]  
-	then
-		echo "Reverting to nvm default version"
-		nvm use default
-	fi
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
+      nvm use
+    fi
+  elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
 }
 
-if [[ "$ZSH_NVM_AUTOLOAD" == "true" ]]
-then
-	autoload -U add-zsh-hook
-	add-zsh-hook chpwd load-nvmrc
-	load-nvmrc
-fi
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
